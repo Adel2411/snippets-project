f24c802e42fa239e31bfbe03783327f267632caa
Adel2411 <hadjarabadel.2411@gmail.com>
2025-01-22 01:18:40 +0100

    Create form for creating snippets

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function SnippetCreatePage() {
-  return <div>Create a snippet</div>;
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a new snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12">Title</label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12">Code</label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-slate-800">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default SnippetCreatePage;
