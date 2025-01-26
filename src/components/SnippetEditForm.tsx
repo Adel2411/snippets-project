f88a2f21621fe7ef13dbd2179a5b42967c42043f
Adel2411 <hadjarabadel.2411@gmail.com>
2025-01-26 05:46:23 +0100

    Added the monaco editor to the edit page

--- src/components/SnippetEditForm.tsx
+++ src/components/SnippetEditForm.tsx
@@ -1,19 +1,38 @@
 "use client";
 
+import { Editor } from "@monaco-editor/react";
 import { Snippet } from "@prisma/client";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+
+  function handleEditorChange(value: string = "") {
+    setCode(value);
+  }
+
   return (
     <div>
       <h1 className="font-bold text-2xl m-4">Snippet Edit Form !</h1>
       <h3 className="font-bold">{snippet.title}</h3>
-      <pre className="flex flex-col gap-8 border rounded-md p-2 m-4 bg-gradient-to-br from-slate-100 to-gray-200 shadow-md">
-        <code>{snippet.code}</code>
-      </pre>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        defaultLanguage="typescript"
+        defaultValue={code}
+        onChange={handleEditorChange}
+        options={{
+          minimap: { enabled: false },
+          fontSize: 16,
+          wordWrap: "on",
+          wrappingIndent: "same",
+          wrappingStrategy: "advanced",
+        }}
+      />
     </div>
   );
 }
